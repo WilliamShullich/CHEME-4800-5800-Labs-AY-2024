a0147b7fc17e1d55e407d31797109351086c512d
WilliamShullich <wes232@cornell.edu>
2024-01-30 13:38:06 -0500

    Update HelloWorld.py

--- week-1/Lab-1d/HelloWorld.py
+++ week-1/Lab-1d/HelloWorld.py
@@ -3,5 +3,5 @@ def printgreeting():
 
 # There is alot of confusion about the if __name__ == '__main__' statement. 
 # This statement is used to check whether the module is being run directly or being imported into another module or program.
-# if __name__ == '__main__':
-#     print(printgreeting())
+if __name__ == '__main__':
+    print(printgreeting())
